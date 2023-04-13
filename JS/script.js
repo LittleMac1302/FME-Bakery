c41db23922c8cb37240722b65f6fcfa3233ca8b9
Fraser Mclennan <frasermclennan1302@gmail.com>
2023-04-14 00:02:26 +0100

    14/4

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,20 @@
-<script>
-  function myFunction(x) {
+function myFunction(x) {
   x.classList.toggle("change");
-  }
+}
 
-  function openPop() {
-    document.getElementById('pop-menu').style.width="250px";
-  }
+function openPop() {
+  document.getElementById('pop-menu').style.width="250px";
+}
 
-  function closePop() {
-    document.getElementById('pop-menu').style.width="0px";
+function closePop() {
+  document.getElementById('pop-menu').style.width="0px";
+}
+
+function popFunction(){
+  var sideOpen = false;
+  if(sideOpen == false){
+    openPop();
+  } else if (sideOpen == true) {
+    closePop();
   }
-  </script>
+}
