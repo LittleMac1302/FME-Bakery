7f392c6af21db01203b44ff3c5a47a2daa7e8155
Fraser Mclennan <frasermclennan1302@gmail.com>
2023-04-13 17:10:39 +0100

    Fixed issue

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+<script>
+  function myFunction(x) {
+  x.classList.toggle("change");
+  }
+
+  function openPop() {
+    document.getElementById('pop-menu').style.width="250px";
+  }
+
+  function closePop() {
+    document.getElementById('pop-menu').style.width="0px";
+  }
+  </script>
